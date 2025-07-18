e1d19fb961441a71dace007400c62bd05c5b1ce0
abolfazll1382 <163577487+abolfazll1382@users.noreply.github.com>
2025-07-18 11:53:04 +0330

    Delete whisper directory

--- whisper/forms.py
+++
@@ -1,4 +0,0 @@
-from django import forms
-
-class AudioUploadForm(forms.Form):
-    audio_file = forms.FileField(label='Select an audio file', widget=forms.ClearableFileInput(attrs={'accept': 'audio/*'}))
