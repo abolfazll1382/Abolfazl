c4d029e1cc90b899ed1b06d736670c2e5a46fab3
abolfazll1382 <163577487+abolfazll1382@users.noreply.github.com>
2025-07-19 10:10:45 +0330

    Delete whisper directory

--- whisper/apps.py
+++
@@ -1,9 +0,0 @@
-from django.apps import AppConfig
-
-class VoiceToTextConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'voice_to_text'
-
-    def ready(self):
-        # Place to import signals if needed
-        pass
